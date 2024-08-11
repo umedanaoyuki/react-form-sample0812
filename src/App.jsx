49e9eb74c56dcb644efe25d6cf543625d9e8e7db
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-12 02:48:27 +0900

    フォームの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <>テスト</>;
-}
+export default function StateForm() {
+  // フォームとして扱う値をStateとして宣言
+  const [form, setForm] = useState({
+    name: "山田太郎",
+    age: 18,
+  });
 
-export default App;
+  // フォーム要素の変更をStateに反映
+  const handleForm = (e) => {
+    setForm({
+      ...form,
+      // ここはなに？
+      [e.target.value]: e.target.value,
+    });
+  };
+
+  const show = () => {
+    console.log(`こんにちは、${form.name}(${form.age}歳)さん！`);
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">名前：</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={handleForm}
+          value={form.name}
+        />
+      </div>
+      <div>
+        <label htmlFor="age">年齢：</label>
+        <input
+          id="age"
+          name="age"
+          type="number"
+          onChange={handleForm}
+          value={form.age}
+        />
+      </div>
+      <div>
+        <button type="button" onClick={show}>
+          送信
+        </button>
+      </div>
+      <p>
+        こんにちは、{form.name}({form.age}歳)さん！
+      </p>
+    </form>
+  );
+}
